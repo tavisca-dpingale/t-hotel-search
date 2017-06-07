7e673c07d08a61b36b9c2b5501f950f68d267213
gpawade <gpawade@tavisca.com>
2017-06-07 08:22:10 +0530

    fixed the deployment issues

--- gpages_build.sh
+++ gpages_build.sh
@@ -1,15 +1,32 @@
-#!/bin/bash -e
+#
+# Modified to work with Travis CI automated builds
+# Original license follows
+#
+# @license
+# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+#
+
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
 
 # usage gp Polymer core-item [branch]
 # Run in a clean directory passing in a GitHub org and repo name
 org=$1
 repo=$2
-branch=${3:-"master"} # default to master when branch isn't specified
+name=$3
+email=$4
+branch=${5:-"master"} # default to master when branch isn't specified
 
 mkdir temp && cd temp
+
 # make folder (same as input, no checking!)
 mkdir $repo
-git clone git@github.com:$org/$repo.git --single-branch
+git clone "https://${GH_TOKEN}@${GH_REF}" --single-branch
 
 # switch to gh-pages branch
 pushd $repo >/dev/null
@@ -34,9 +51,12 @@ mv demo components/$repo/
 # redirect by default to the component folder
 echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
 
+git config user.name $name
+git config user.email $email
+
 # send it all to github
 git add -A .
-git commit -am 'seed gh-pages'
-git push -u origin gh-pages --force
+git commit -am 'Deploy to GitHub Pages'
+git push --force --quiet -u "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
 
 popd >/dev/null
